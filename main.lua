5e19960176bd8a507cd9583c31e9d8ff2be85ddb
DrOtter3000 <info@darknet-online.com>
2025-08-28 19:07:59 +0200

    add jump and run mechanics

--- main.lua
+++ main.lua
@@ -1,23 +1,39 @@
 function love.load()
     wf = require 'libraries/windfield/windfield'
-    world = wf.newWorld(0, 800)
+    world = wf.newWorld(0, 800, false)
+    world:setQueryDebugDrawing(true)
 
-    player = world:newRectangleCollider(360, 100, 80, 80)
+    world:addCollisionClass('Player')
+    world:addCollisionClass('Platform')
+    world:addCollisionClass('Danger')
+
+    player = world:newRectangleCollider(360, 100, 80, 80, {collision_class = "Player"})
+    player:setFixedRotation(true)
     player.speed = 500
-    platform = world:newRectangleCollider(250, 400, 300, 100)
+
+    platform = world:newRectangleCollider(250, 400, 300, 100, {collision_class = "Platform"})
     platform:setType("static")
+
+    dangerZone = world:newRectangleCollider(0, 550, 800, 50, {collision_class = "Danger"})
+    dangerZone:setType("static")
 end
 
 
 function love.update(dt)
     world:update(dt)
 
-    local px, py = player:getPosition()
-    if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
-        player:setX(px + player.speed * dt)
-    end
-    if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
-        player:setX(px - player.speed * dt)
+    if player.body then
+        local px, py = player:getPosition()
+        if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
+            player:setX(px + player.speed * dt)
+        end
+        if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
+            player:setX(px - player.speed * dt)
+        end
+
+        if player:enter('Danger') then
+            player:destroy()
+        end
     end
 end
 
@@ -29,6 +45,21 @@ end
 
 function love.keypressed( key )
     if key == 'w' or key == 'up' or key == 'space' then
-        player:applyLinearImpulse(0, -7000)
+        local colliders = world:queryRectangleArea(player:getX() - 40, player:getY() + 40, 80, 2, {'Platform'})
+        if #colliders > 0 then
+            player:applyLinearImpulse(0, -7000)
+    
+        end
+
+    end
+end
+
+
+function love.mousepressed(x, y, button)
+    if button == 1 then
+        local colliders = world:queryCircleArea(x, y, 50, {'Platform', 'Danger'})
+        for i,c in ipairs(colliders) do
+            c:destroy()
+        end
     end
 end
