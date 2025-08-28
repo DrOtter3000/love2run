00e6c1dcee448f0c374fa409f65cdb0694f5de2a
DrOtter3000 <info@darknet-online.com>
2025-08-28 16:55:54 +0200

    build basic world

---
+++ main.lua
@@ -0,0 +1,34 @@
+function love.load()
+    wf = require 'libraries/windfield/windfield'
+    world = wf.newWorld(0, 800)
+
+    player = world:newRectangleCollider(360, 100, 80, 80)
+    player.speed = 500
+    platform = world:newRectangleCollider(250, 400, 300, 100)
+    platform:setType("static")
+end
+
+
+function love.update(dt)
+    world:update(dt)
+
+    local px, py = player:getPosition()
+    if love.keyboard.isDown('right') or love.keyboard.isDown('d') then
+        player:setX(px + player.speed * dt)
+    end
+    if love.keyboard.isDown('left') or love.keyboard.isDown('a') then
+        player:setX(px - player.speed * dt)
+    end
+end
+
+
+function love.draw()
+    world:draw()
+end
+
+
+function love.keypressed( key )
+    if key == 'w' or key == 'up' or key == 'space' then
+        player:applyLinearImpulse(0, -7000)
+    end
+end
